b4728c5551510736cb89149d5f5ec68c92f36068
Geeta-Choudhary <geetaseervi.2002@gmail.com>
2025-03-29 15:18:39 +0530

    assignments

---
+++ .NET/StudentManagementSystem/StudentManagementSystem/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagementSystem.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
+
