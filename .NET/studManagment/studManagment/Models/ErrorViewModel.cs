983a62ba19c2cc4d0ac836248740b6daf198c20b
Geeta-Choudhary <geetaseervi.2002@gmail.com>
2025-03-29 15:21:20 +0530

    assignments modifed

--- .NET/studManagment/studManagment/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace studManagment.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
