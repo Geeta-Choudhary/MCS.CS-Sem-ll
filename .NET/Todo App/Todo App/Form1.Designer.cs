c566407b72dcbad158040f0746afc6f0c56466f5
Geeta-Choudhary <geetaseervi.2002@gmail.com>
2025-03-29 07:48:59 +0530

    A console calculator

---
+++ .NET/Todo App/Todo App/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Todo_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            radioButton1 = new RadioButton();
+            label3 = new Label();
+            label4 = new Label();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            monthCalendar1 = new MonthCalendar();
+            button1 = new Button();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.InactiveCaption;
+            textBox1.Location = new Point(418, 330);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(452, 46);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.InactiveCaption;
+            textBox2.Location = new Point(418, 395);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(452, 46);
+            textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AllowDrop = true;
+            label1.AutoSize = true;
+            label1.Location = new Point(202, 346);
+            label1.Name = "label1";
+            label1.Size = new Size(181, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Enter the Task Name :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(202, 411);
+            label2.Name = "label2";
+            label2.Size = new Size(181, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Enter the Task Name :";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(915, 416);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(21, 20);
+            radioButton1.TabIndex = 4;
+            radioButton1.TabStop = true;
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.YellowGreen;
+            label3.ImageAlign = ContentAlignment.BottomLeft;
+            label3.Location = new Point(897, 281);
+            label3.Name = "label3";
+            label3.Size = new Size(55, 25);
+            label3.TabIndex = 5;
+            label3.Text = "Done";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.YellowGreen;
+            label4.Location = new Point(995, 281);
+            label4.Name = "label4";
+            label4.Size = new Size(90, 25);
+            label4.TabIndex = 6;
+            label4.Text = "Not Done";
+            label4.Click += label4_Click;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(1025, 416);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(21, 20);
+            radioButton2.TabIndex = 7;
+            radioButton2.TabStop = true;
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(1025, 353);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(21, 20);
+            radioButton3.TabIndex = 8;
+            radioButton3.TabStop = true;
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(915, 348);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(21, 20);
+            radioButton4.TabIndex = 9;
+            radioButton4.TabStop = true;
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.Location = new Point(497, 65);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.MenuHighlight;
+            button1.Location = new Point(592, 484);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 70);
+            button1.TabIndex = 11;
+            button1.Text = "Submit";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.InactiveCaption;
+            textBox3.Location = new Point(537, 12);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(210, 46);
+            textBox3.TabIndex = 12;
+            textBox3.Text = "TO DO APP";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1159, 630);
+            Controls.Add(textBox3);
+            Controls.Add(button1);
+            Controls.Add(monthCalendar1);
+            Controls.Add(radioButton4);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(radioButton1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private RadioButton radioButton1;
+        private Label label3;
+        private Label label4;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private MonthCalendar monthCalendar1;
+        private Button button1;
+        private TextBox textBox3;
+    }
+}
